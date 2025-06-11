77aa40fc4b84808a3d2d0e17471dacea6c7ee0ed
bsonju1 <bsonju1@wgu.edu>
2025-06-11 08:34:40 -0700

    Part H: Checkout controller with Rest and postmapping

---
+++ src/main/java/edu/wgu/d288/controllers/CheckoutController.java
@@ -0,0 +1,24 @@
+package edu.wgu.d288.controllers;
+
+import edu.wgu.d288.services.CheckoutService;
+import edu.wgu.d288.services.Purchase;
+import edu.wgu.d288.services.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("http://localhost:8080/api/checkout")
+@CrossOrigin(origins = "http://localhost:4200")
+
+        public class CheckoutController {
+
+        @Autowired
+        private CheckoutService checkoutService;
+
+        @PostMapping("/purchase")
+        public ResponseEntity<PurchaseResponse> placeOrder(@RequestBody Purchase purchase) {
+        PurchaseResponse response = checkoutService.placeOrder(purchase);
+        return ResponseEntity.ok(response);
+        }
+        }
