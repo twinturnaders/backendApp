a84464d9171290c895ffe1e55a000c5722a22ac5
bsonju1 <bsonju1@wgu.edu>
2025-06-11 06:24:59 -0700

    Part B: Initial Spring App and dependencies

---
+++ src/main/java/edu/wgu/d288/D288Application.java
@@ -0,0 +1,13 @@
+package edu.wgu.d288;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D288Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D288Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/wgu/d288/D288ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wgu.d288;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D288ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
